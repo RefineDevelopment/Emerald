babbe1d0a1a265ab8259d96bddcba495712f66c0
steamworksmc <58116838+steamworksmc@users.noreply.github.com>
2021-09-05 15:34:42 -0400

    Update SharedEmerald.java

--- Shared/src/main/java/me/zowpy/emerald/shared/SharedEmerald.java
+++ Shared/src/main/java/me/zowpy/emerald/shared/SharedEmerald.java
@@ -34,20 +34,21 @@ public class SharedEmerald {
     private final ServerManager serverManager;
     private final GroupManager groupManager;
 
-    @Setter private ServerProperties serverProperties;
+    @Setter 
+    private ServerProperties serverProperties;
 
     public SharedEmerald(UUID uuid, RedisCredentials credentials) {
         this.uuid = uuid;
-        this.jedisAPI = new JedisAPI(credentials);
-        jedisAPI.registerSubscriber(new SharedJedisSubscriber(this));
-
+        
         this.serverManager = new ServerManager(this);
         this.groupManager = new GroupManager();
-
+        
+        (this.jedisAPI = new JedisAPI(credentials)).registerSubscriber(new SharedJedisSubscriber(this));
     }
 
     public void executeCommand(EmeraldServer server, String command) {
         JsonObject jsonObject = new JsonObject();
+        
         jsonObject.addProperty("command", command);
         jsonObject.addProperty("uuid", server.getUuid().toString());
 
